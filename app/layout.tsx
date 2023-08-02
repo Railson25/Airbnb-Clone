b9ee6d3aaeb5d0a8ea8385e4bcf2d2d97165a6c1
Railson25 <railsonsantiago123@gmail.com>
2023-08-02 19:34:10 -0300

    <style>: Font project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
+import { Navbar } from './components/navbar/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
