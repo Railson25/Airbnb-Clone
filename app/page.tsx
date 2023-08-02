211e343741d7692c4b193396dc8dd9db3780f5b0
Railson25 <railsonsantiago123@gmail.com>
2023-08-02 19:38:27 -0300

    <feat>:   Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className='flex items-center justify-center'>
-      Landing
+    <div className='flex items-center justify-center h-full '>
+      <div className="flex items-center justify-between gap-3 md:gap-0">
+
+      </div>
     </div>
   )
 }
