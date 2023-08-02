0e04b9605e859b8a153c3981934e35e9fe0b11c5
Railson25 <railsonsantiago123@gmail.com>
2023-08-02 19:38:10 -0300

    <feat>:  Navbar component

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import { Container } from "../container"
+import { Logo } from "./logo"
+import { Search } from "./search"
+import { UserMenu } from "./user-menu"
+
+export const Navbar = () => {
+    return(
+        <div className="fixed w-full bg-white z-10 shadow-sm  text-gray-600 ">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
+                        <Logo />
+                        <Search />
+                        <UserMenu />
+                    </div>
+                </Container>
+            </div>
+        </div>
+    )
+}  
