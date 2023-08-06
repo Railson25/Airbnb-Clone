f5c0efa185538ec343dd1f2dccfb29e7bbf34feb
Railson25 <railsonsantiago123@gmail.com>
2023-08-06 20:47:15 -0300

    <feat>: Navbar types

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import type { Metadata } from 'next'
 import ClientOnly  from './components/client-only'
 import { RegisterModal } from './components/modals/register-modal'
 import { ToasterProvider } from './providers/ToasterProvider'
+import { LoginModal } from './components/modals/login-modal'
+import getCurrentUser from './actions/getCurrentUser'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -14,18 +16,21 @@ export const metadata: Metadata = {
   description: 'Airbnb clone',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const currentUser = await getCurrentUser()
+    
   return (
     <html lang="en">
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider/>
-          <Navbar />
+          <Navbar currentUser={currentUser} />
           <RegisterModal />
+          <LoginModal />
         </ClientOnly>
         {children}
         </body>
