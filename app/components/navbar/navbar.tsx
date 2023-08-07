7e78aec6c6dfdf0e5d4e3240566a550f17c18077
Railson25 <railsonsantiago123@gmail.com>
2023-08-07 18:58:21 -0300

    <feat> adding category to navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Container } from "../container"
+import { Categories } from "./categories"
 import { Logo } from "./logo"
 import { Search } from "./search"
 import { UserMenu } from "./user-menu"
@@ -23,6 +24,7 @@ export const Navbar = ({currentUser}: NavbarProps) => {
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     )
 }  
