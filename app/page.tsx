bb7a4195b2941cf51653e6d1c869fd37bcf88883
Railson25 <railsonsantiago123@gmail.com>
2023-08-10 18:17:44 -0300

    <feat>: listing function

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,39 @@
-
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/client-only";
+import { Container } from "./components/container";
+import { EmptyState } from "./components/empty-state";
+import { ListingCard } from "./components/listings/listing-card ";
 
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
+ 
+  if(listings.length === 0){
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
 
   return (
     <ClientOnly >
-      <div className='flex items-center justify-center h-full '>
-        <div className="flex items-center justify-between gap-3 md:gap-0">
-          
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 
+          md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
+          {listings.map((listing: any) => {
+            return(
+              <ListingCard 
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}
         </div>
-      </div>
-
+      </Container>
     </ClientOnly>
   )
 }
