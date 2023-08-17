f591698f4d123367ef64232ff08b44ad38b0e90d
Railson25 <railsonsantiago123@gmail.com>
2023-08-17 13:13:03 -0300

    <feat>: adding all filters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -3,13 +3,70 @@ import prisma from '@/app/libs/prismadb'
 
 export interface IListingsParams {
     userId?: string
+    guestCount?: number
+    roomCount?: number
+    bathroomCount?: number
+    startDate?: string
+    endDate?: string
+    locationValues?: string
+    category?: string
 }
 
-export default  async function getListings({userId}: IListingsParams) {
+export default  async function getListings({
+    userId, bathroomCount, category, endDate,
+    guestCount, locationValues, roomCount, startDate
+}: IListingsParams) {
+
     try {
         let query: any = {}
+
         if(userId) {
             query.userId = userId
+        }
+
+        if(category) {
+            query.category = category
+        }
+
+        if(roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if(bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if(guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+
+        if(locationValues){
+            query.locationValue = locationValues
+        }
+
+        if(startDate && endDate){
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: {gte: startDate},
+                                startDate: {lte: startDate},
+                            },
+                            {
+                                startDate: {lte: endDate},
+                                endDate: {gte: endDate},
+                            }
+                        ]
+                    }
+                }
+            }
         }
 
         const listings = await prisma.listing.findMany({
